8a1a5d43ebe0cedec193c578d493ccaa842f1086
Stijn <93727434+scosterus@users.noreply.github.com>
2022-01-12 22:15:31 +0100

    Color changing JS added

--- resources/js/script.js
+++ resources/js/script.js
@@ -5,4 +5,20 @@ document.addEventListener('mousemove', e => {
     
     cursor.style.left = e.clientX + 'px';
     cursor.style.top = e.clientY + 'px';
+
+    //calculate width of the page
+    let width = window.innerWidth
+    || document.documentElement.clientWidth
+    || document.body.clientWidth;
+
+    var height = window.innerHeight
+    || document.documentElement.clientHeight
+    || document.body.clientHeight;
+
+    let colorX = e.clientX / width * 200;
+    let colorY = e.clientY / height * 200;
+    let colorHue = Math.floor(colorX + colorY);
+
+    console.log(colorHue)
+    cursor.style.backgroundImage = `linear-gradient(90deg, hsla(${colorHue}, 100%, 50%, 1), hsla(${colorHue + 30}, 100%, 50%, 1))`;
 })
