519d2cd49a91ffd19d37ea2ba152884149da3d70
Stijn <93727434+scosterus@users.noreply.github.com>
2022-01-12 17:47:42 +0100

    buggy and incorrect cursor animation fixed

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,6 +1,8 @@
 // reverse mouse hover script for the hero
 const cursor = document.querySelector('.cursor');
 document.addEventListener('mousemove', e => {
+    e.stopPropagation();
+    
     cursor.style.left = e.clientX + 'px';
     cursor.style.top = e.clientY + 'px';
 })
