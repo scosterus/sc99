7bebd9c77ce95ef480990bc08dc707856a6d9f2b
Stijn <93727434+scosterus@users.noreply.github.com>
2022-01-13 11:52:40 +0100

    JS now sticks to hero section of the site

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,24 +1,42 @@
 // reverse mouse hover script for the hero
 const cursor = document.querySelector('.cursor');
+
 document.addEventListener('mousemove', e => {
     e.stopPropagation();
-    
-    cursor.style.left = e.clientX + 'px';
-    cursor.style.top = e.clientY + 'px';
 
-    //calculate width of the page
+    //calculate width and height of the page
     let width = window.innerWidth
     || document.documentElement.clientWidth
     || document.body.clientWidth;
 
-    var height = window.innerHeight
+    let height = window.innerHeight
     || document.documentElement.clientHeight
     || document.body.clientHeight;
+
+    let scrollTop = window.pageYOffset || (document.documentElement || document.body.parentNode || document.body).scrollTop
+
+    let maxHeight = height - scrollTop;
+
+    //console.log(maxHeight)
+    
+    if(e.clientY < maxHeight) {
+        cursor.style.top = e.clientY + 'px';
+        cursor.style.height = '';
+        cursor.style.width = '';
+    }
+
+    if(e.clientY > maxHeight) {
+        cursor.style.top = maxHeight + 'px';
+        cursor.style.height = '10px';
+        cursor.style.width = '10px';
+    }
+
+
+    cursor.style.left = e.clientX + 'px';
 
     let colorX = e.clientX / width * 200;
     let colorY = e.clientY / height * 200;
     let colorHue = Math.floor(colorX + colorY);
 
-    console.log(colorHue)
     cursor.style.backgroundImage = `linear-gradient(90deg, hsla(${colorHue}, 100%, 50%, 1), hsla(${colorHue + 30}, 100%, 50%, 1))`;
 })
